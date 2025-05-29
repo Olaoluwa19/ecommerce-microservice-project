350f5b4204c470d2cd9f22db650a4749bcc343db
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-05-29 06:36:17 +0100

    added create account function

--- user-service/app/repository/userRepository.ts
+++ user-service/app/repository/userRepository.ts
@@ -1,9 +1,20 @@
 import { UserModel } from "../models/UserModel.js";
+import { DBClient } from "../utility/databaseClient.js";
 
 export class UserRepository {
   constructor() {}
 
   async createAccount({ email, password, salt, phone, userType }: UserModel) {
-    console.log("User created in DB");
+    const client = DBClient();
+    await client.connect();
+
+    const querryString =
+      "INSERT INTO users (phone, email, password, salt, user_type) VALUES($1, $2, $3, $4, $5)";
+    const values = [phone, email, password, salt, userType];
+    const result = await client.query(querryString, values);
+    await client.end();
+    if (result.rowCount > 0) {
+      return result.rows[0] as UserModel;
+    }
   }
 }
