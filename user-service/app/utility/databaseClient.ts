d7f73ee9f3c9ad30460c9914d6b6a74d4fe68320
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-09-24 03:21:01 +0100

    tried my possible best to connect to docker user_service db. but it failed severally so i had to use pgAdmin4 default db

--- user-service/app/utility/databaseClient.ts
+++ user-service/app/utility/databaseClient.ts
@@ -2,24 +2,10 @@ import { Client } from "pg";
 
 export const DBClient = () => {
   return new Client({
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    host: "127.0.0.1",
+    user: "root",
+    database: "user_service",
+    password: "root",
     port: 5432,
   });
 };
-
-// import { Pool } from "pg";
-
-// const pool = new Pool({
-//   host: process.env.POSTGRES_HOST,
-//   user: process.env.POSTGRES_USER,
-//   database: process.env.POSTGRES_DB,
-//   password: process.env.POSTGRES_PASSWORD,
-//   port: 5432,
-//   max: 20, // Maximum number of connections in the pool
-//   idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
-// });
-
-// export const DBClient = () => pool;
