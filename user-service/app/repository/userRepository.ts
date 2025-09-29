51457b1e62477cbeeacaa6cf1477d690ce15bec9
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-09-29 06:58:02 +0100

    performed cleanup  for userRespository to extend DBOperation

--- user-service/app/repository/userRepository.ts
+++ user-service/app/repository/userRepository.ts
@@ -1,18 +1,17 @@
 import { UserModel } from "../models/UserModel.js";
 import { DBClient } from "../utility/databaseClient.js";
+const { DBOperation } = await import("./dbOperation.js");
 
-export class UserRepository {
-  constructor() {}
+export class UserRepository extends DBOperation {
+  constructor() {
+    super();
+  }
 
   async createAccount({ email, password, salt, phone, userType }: UserModel) {
-    const client = DBClient();
-    await client.connect();
-
     const queryString =
       "INSERT INTO users (phone, email, password, salt, user_type) VALUES($1, $2, $3, $4, $5) RETURNING *"; //user_id, email, phone, user_type
     const values = [phone, email, password, salt, userType];
-    const result = await client.query(queryString, values);
-    await client.end();
+    const result = await this.executeQuery(queryString, values);
     if (result.rowCount > 0) {
       return result.rows[0] as UserModel;
     }
@@ -20,17 +19,24 @@ export class UserRepository {
   }
 
   async findAccount(email: string) {
-    const client = DBClient();
-    await client.connect();
-
     const queryString =
       "SELECT user_id, email, phone, password, salt FROM users WHERE email = $1";
     const values = [email];
-    const result = await client.query(queryString, values);
-    await client.end();
+    const result = await this.executeQuery(queryString, values);
     if (result.rowCount < 1) {
       throw new Error("User does not exist with provided email id!");
     }
     return result.rows[0] as UserModel;
+  }
+
+  async updateVerificationCode(userId: string, code: number, expiry: Date) {
+    const queryString =
+      "UPDATE users SET verification_code=$1, expiry=$2 WHERE user_id=$3 RETURNING *"; //user_id, email, phone, user_type
+    const values = [code, expiry, userId];
+    const result = await this.executeQuery(queryString, values);
+    if (result.rowCount > 0) {
+      return result.rows[0] as UserModel;
+    }
+    throw new Error("Failed to create user");
   }
 }
