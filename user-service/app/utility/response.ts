57b28a4c2ea2d8cc0ec24fb26856b8215134f866
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-10-04 03:06:57 +0100

    improve the robustness of formatResponse

--- user-service/app/utility/response.ts
+++ user-service/app/utility/response.ts
@@ -1,28 +1,25 @@
-const formatResponse = (statusCode: number, message: string, data: unknown) => {
-  if (data) {
-    return {
-      statusCode,
-      headers: {
-        "Access-Control-Origin": "*",
-      },
-      body: JSON.stringify({
-        message,
-        data,
-      }),
-    };
-  } else {
-    return {
-      statusCode,
-      headers: {
-        "Access-Control-Origin": "*",
-      },
-      body: JSON.stringify({
-        message,
-      }),
-    };
-  }
+const formatResponse = (
+  statusCode: number,
+  message: string,
+  data?: unknown
+) => {
+  const response = {
+    statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Corrected header name
+      "Content-Type": "application/json", // Explicitly set Content-Type
+      "Access-Control-Allow-Headers": "Content-Type", // Optional: for CORS
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET", // Optional: for CORS
+    },
+    body: JSON.stringify({
+      message,
+      ...(data && { data }), // Conditionally include data if provided
+    }),
+  };
+  return response;
 };
 
+export default formatResponse;
 export const SuccessResponse = (data: object) => {
   return formatResponse(200, "Success", data);
 };
@@ -32,9 +29,9 @@ export const ErrorResponse = (code = 1000, error: unknown) => {
     const errorObject = error[0].constraints;
     const errorMessage =
       errorObject[Object.keys(errorObject)[0]] || "Error Occured";
-      console.log(error)
+    console.log(error);
     return formatResponse(code, errorMessage, errorMessage);
   }
-  console.log(error)
+  console.log(error);
   return formatResponse(code, `${error}`, error);
 };
