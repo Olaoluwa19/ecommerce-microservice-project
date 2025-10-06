77bf967a15134f3b0dc717d93e3ce7410693ae5f
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-10-06 03:33:59 +0100

    added new script and commented out some old script

--- user-service/migrations/sqls/20251006013814-user-address-down.sql
+++ user-service/migrations/sqls/20251006013814-user-address-down.sql
@@ -1,4 +1,6 @@
-DROP INDEX IF EXISTS address_city_idx;
-DROP INDEX IF EXISTS address_post_code_idx;
-DROP INDEX IF EXISTS address_country_idx;
-DROP TABLE IF EXISTS address;
+ALTER TABLE IF EXISTS "address" DROP CONSTRAINT IF EXISTS "address_user_id_fkey";
+
+DROP TABLE IF EXISTS "address";
+-- DROP INDEX IF EXISTS address_city_idx;
+-- DROP INDEX IF EXISTS address_post_code_idx;
+-- DROP INDEX IF EXISTS address_country_idx;
