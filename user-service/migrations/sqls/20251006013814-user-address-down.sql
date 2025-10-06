a2d6b980b4744d91d3fc593ecfab58ea2c9fe7e8
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-10-06 03:28:44 +0100

    created adress migration and added scripts in it

--- user-service/migrations/sqls/20251006013814-user-address-down.sql
+++ user-service/migrations/sqls/20251006013814-user-address-down.sql
@@ -1,1 +1,4 @@
-/* Replace with your SQL commands */
+DROP INDEX IF EXISTS address_city_idx;
+DROP INDEX IF EXISTS address_post_code_idx;
+DROP INDEX IF EXISTS address_country_idx;
+DROP TABLE IF EXISTS address;
--- user-service/migrations/sqls/20251006013814-user-address-up.sql
+++ user-service/migrations/sqls/20251006013814-user-address-up.sql
@@ -1,1 +1,17 @@
-/* Replace with your SQL commands */
+CREATE TABLE IF NOT EXISTS "address" (
+    "id" bigserial PRIMARY KEY,
+    "user_id" bigint NOT NULL,
+    "address_line1" text,
+    "address_line2" text,
+    "city" varchar,
+    "post_code" integer,
+    "country" varchar,
+    "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX IF NOT EXISTS address_city_idx ON "address" ("city");
+CREATE INDEX IF NOT EXISTS address_post_code_idx ON "address" ("post_code");
+CREATE INDEX IF NOT EXISTS address_country_idx ON "address" ("country");
+
+-- add Relation
+ALTER TABLE "address" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
