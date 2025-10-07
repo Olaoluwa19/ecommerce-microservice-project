1ea140e18de127129d67aecaa415e15a92d02b43
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-10-07 02:14:24 +0100

    modified user_id to numbe, made first_name and last_name optional

--- user-service/app/models/UserModel.ts
+++ user-service/app/models/UserModel.ts
@@ -1,12 +1,12 @@
 export interface UserModel {
-  user_id?: string;
+  user_id?: number;
   email: string;
   password: string;
   salt: string;
   phone: string;
   userType: "BUYER" | "SELLER";
-  first_name: string;
-  last_name: string;
+  first_name?: string;
+  last_name?: string;
   profile_pic?: string;
   verification_code?: number;
   expiry?: Date;
