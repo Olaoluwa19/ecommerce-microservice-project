4045f9397502abf9b3ecff51c5c2c9deb9e4d2dc
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-10-07 02:03:57 +0100

    clean up commented scripts

--- user-service/migrations/sqls/20251006013814-user-address-down.sql
+++ user-service/migrations/sqls/20251006013814-user-address-down.sql
@@ -1,6 +1,3 @@
 ALTER TABLE IF EXISTS "address" DROP CONSTRAINT IF EXISTS "address_user_id_fkey";
 
 DROP TABLE IF EXISTS "address";
--- DROP INDEX IF EXISTS address_city_idx;
--- DROP INDEX IF EXISTS address_post_code_idx;
--- DROP INDEX IF EXISTS address_country_idx;
