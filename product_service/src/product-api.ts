fb88011dd2537d166036f43a5ea791c110f89af6
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-11-03 04:53:23 +0100

    added switch satement to handle  requests in handler

--- product_service/src/product-api.ts
+++ product_service/src/product-api.ts
@@ -1,16 +1,29 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ErrorResponse } from "./utility/response";
 
 export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  console.log(`Event: ${JSON.stringify(event)}`);
-  console.log(`Context: ${JSON.stringify(context)}`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Product Service Lambda!",
-      path: `${event.path}, ${event.pathParameters}`,
-    }),
-  };
+  const isRoot = event.pathParameters === null
+
+  switch (event.httpMethod.toLowerCase()) {
+    case "post":
+      if (isRoot) {
+        //call create product service
+      }
+      break;
+    case "get": 
+      // return isRoot ? " // call get Products" : "call get product by id"
+    case "put":
+      if (!isRoot) {
+        // call edit product
+      }
+    case "delete":
+      if (!isRoot) {
+        // delete product
+      }
+  }
+
+  return ErrorResponse(404, "requetsted method not allowed")
 };
