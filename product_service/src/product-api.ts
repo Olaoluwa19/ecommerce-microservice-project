e9c9c7ab75f3a3a70ce5aa2044b59850237e2a6f
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-11-03 05:21:14 +0100

    added endpoints to product api

--- product_service/src/product-api.ts
+++ product_service/src/product-api.ts
@@ -1,20 +1,24 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { ErrorResponse } from "./utility/response";
+import { ProductService } from "./service/product-service";
+import { ProductRepository } from "./repository/product-repository";
+
+const service = new ProductService(new ProductRepository());
 
 export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  const isRoot = event.pathParameters === null
+  const isRoot = event.pathParameters === null;
 
   switch (event.httpMethod.toLowerCase()) {
     case "post":
       if (isRoot) {
-        //call create product service
+        return service.createProduct();
       }
       break;
-    case "get": 
-      // return isRoot ? " // call get Products" : "call get product by id"
+    case "get":
+    // return isRoot ? " // call get Products" : "call get product by id"
     case "put":
       if (!isRoot) {
         // call edit product
@@ -25,5 +29,5 @@ export const handler = async (
       }
   }
 
-  return ErrorResponse(404, "requetsted method not allowed")
+  return ErrorResponse(404, "requetsted method not allowed");
 };
