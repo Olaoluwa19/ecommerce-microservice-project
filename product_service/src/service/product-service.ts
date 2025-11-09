4ded186b5d34dd11d7d9ff67c3f0a70ae51cacf3
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-11-09 06:03:35 +0100

    started working on createProduct

--- product_service/src/service/product-service.ts
+++ product_service/src/service/product-service.ts
@@ -1,5 +1,9 @@
+import { APIGateWayEvent } from "aws-lambda";
 import { ProductRepository } from "../repository/product-repository";
-import { SuccessResponse } from "../utility/response";
+import { ErrorResponse, SuccessResponse } from "../utility/response";
+import { plainToClass } from "class-transformer";
+import { AppValidationError } from "../utility/errors";
+import { ProductInput } from "../dto/product-input";
 
 export class ProductService {
   _repository: ProductRepository;
@@ -7,23 +11,30 @@ export class ProductService {
     this._repository = repository;
   }
 
-  async createProduct() {
-    return SuccessResponse({ msg: "Product Created!" });
+  async createProduct(event: APIGateWayEvent) {
+    const input = plainToClass(ProductInput, event.body);
+    const error = await AppValidationError(input);
+    console.log(error);
+    if (error) return ErrorResponse(404, error);
+
+    const data = await this._repository.createProduct(input);
+
+    return SuccessResponse(data);
   }
 
-  async getProducts() {
+  async getProducts(event: APIGateWayEvent) {
     return SuccessResponse({ msg: "get Products" });
   }
 
-  async getProduct() {
+  async getProduct(event: APIGateWayEvent) {
     return SuccessResponse({ msg: "get Product by id" });
   }
 
-  async editProduct() {
+  async editProduct(event: APIGateWayEvent) {
     return SuccessResponse({ msg: "edit Product" });
   }
 
-  async deleteProduct() {
+  async deleteProduct(event: APIGateWayEvent) {
     return SuccessResponse({ msg: "delete Product" });
   }
 }
