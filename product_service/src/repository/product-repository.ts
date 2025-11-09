8f6cd9d2820d8f39699b29ea411596c0c8499dee
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-11-09 06:23:47 +0100

    started working on product repo

--- product_service/src/repository/product-repository.ts
+++ product_service/src/repository/product-repository.ts
@@ -1,13 +1,13 @@
 export class ProductRepository {
   constructor() {}
 
-  async createProduct() {}
+  async createProduct({ name, description, price, category_id, image_url }) {}
 
   async getAllProducts(offset = 0, pages?: number) {}
 
   async getProductById(id: string) {}
 
-  async updateProduct() {}
+  async updateProduct({ name, description, price, category_id, image_url }) {}
 
   async deleteProduct(id: string) {}
 }
