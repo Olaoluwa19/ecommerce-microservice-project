8004b8cb71414d75c4481d106422fa323db1a47a
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-11-09 06:23:25 +0100

    created product model

---
+++ product_service/src/models/product-models.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+export type ProductDoc = mongoose.Document & {
+  name: string;
+  description: string;
+  ctategory_id: string;
+  image_url: string;
+  price: number;
+  availability: boolean;
+};
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: String,
+    description: String,
+    ctategory_id: String,
+    image_url: String,
+    price: Number,
+    availability: Boolean,
+  },
+  {
+    toJSON: {
+      transform(doc, ret: { [key: string]: any }, options) {
+        delete ret.__v;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+      },
+    },
+    timestamps: true,
+  }
+);
+
+const products =
+  mongoose.models.products ||
+  mongoose.model<ProductDoc>("products", ProductSchema);
+
+export { products };
