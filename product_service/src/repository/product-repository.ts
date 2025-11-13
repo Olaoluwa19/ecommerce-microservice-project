db7459ae225fabea75ab108e2beafeeec8fa74da
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-11-13 01:49:13 +0100

    started working on createProduct function

--- product_service/src/repository/product-repository.ts
+++ product_service/src/repository/product-repository.ts
@@ -1,13 +1,31 @@
+import { ProductInput } from "../dto/product-input";
+import { products } from "../models/product-models";
+
 export class ProductRepository {
   constructor() {}
 
-  async createProduct({ name, description, price, category_id, image_url }) {}
+  async createProduct({
+    name,
+    description,
+    price,
+    category_id,
+    image_url,
+  }: ProductInput) {
+    return products.create({
+      name,
+      description,
+      price,
+      category_id,
+      image_url,
+      availability: true,
+    });
+  }
 
   async getAllProducts(offset = 0, pages?: number) {}
 
   async getProductById(id: string) {}
 
-  async updateProduct({ name, description, price, category_id, image_url }) {}
+  // async updateProduct({ name, description, price, category_id, image_url }) {}
 
   async deleteProduct(id: string) {}
 }
