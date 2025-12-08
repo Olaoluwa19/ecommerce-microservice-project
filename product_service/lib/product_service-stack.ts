4ade85c3f8687f902b94896fb13de60290ccd270
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-08 07:32:10 +0100

    implemented bucket setting

--- product_service/lib/product_service-stack.ts
+++ product_service/lib/product_service-stack.ts
@@ -2,12 +2,18 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ServiceStack } from "./service-stack";
 import { ApiGatewayStack } from "./api_gateway-stack";
+import { S3BucketStack } from "./s3Bucket-stack";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const { bucket } = new S3BucketStack(scope, "product-bucket");
+
     const { productService, categoryService, dealsService, imageService } =
-      new ServiceStack(this, "ProductService", {});
+      new ServiceStack(this, "ProductService", {
+        bucket: bucket.bucketName,
+      });
     new ApiGatewayStack(this, "ProductApiGateway", {
       productService,
       categoryService,
