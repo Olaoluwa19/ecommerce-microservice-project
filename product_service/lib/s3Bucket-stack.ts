6afaf766deed6a6f4ada5d6bb4282b69855df815
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-08 07:38:46 +0100

    configured s3 bucket stack

---
+++ product_service/lib/s3Bucket-stack.ts
@@ -0,0 +1,25 @@
+import { RemovalPolicies, RemovalPolicy } from "aws-cdk-lib";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketEncryption,
+} from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import { version } from "os";
+
+export class S3BucketStack extends Construct {
+  public readonly bucket: Bucket;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    (this.bucket = new Bucket(scope, `${id}-bucket`)),
+      {
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        encryption: BucketEncryption.S3_MANAGED,
+        enforceSSL: true,
+        versioned: false,
+        RemovalPolicy: RemovalPolicy.DESTROY,
+      };
+  }
+}
