81dd460d0d2633b9ff4304c073e8959da3aa30ae
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-14 18:36:17 +0100

    started setting up the api

--- product_service/src/category-api.ts
+++ product_service/src/category-api.ts
@@ -1,16 +1,41 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ErrorResponse } from "./utility/response";
+import { CategoryService } from "./service/category-service";
+import { CategoryRepository } from "./repository/category-repository";
+import { connectDB } from "./utility/mongodb";
+import middy from "@middy/core";
 
-export const handler = async (
-  event: APIGatewayEvent,
-  context: Context
-): Promise<APIGatewayProxyResult> => {
-  console.log(`Event: ${JSON.stringify(event)}`);
-  console.log(`Context: ${JSON.stringify(context)}`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Category Service Lambda!",
-      path: `${event.path}, ${event.pathParameters}`,
-    }),
-  };
-};
+const service = new CategoryService(new CategoryRepository());
+
+export const handler = middy(
+  (
+    event: APIGatewayEvent,
+    context: Context
+  ): Promise<APIGatewayProxyResult> => {
+    await connectDB();
+
+    const isRoot = event.pathParameters === null;
+
+    switch (event.httpMethod.toLowerCase()) {
+      case "post":
+        if (isRoot) {
+          return service.createCategory(event);
+        }
+        break;
+      case "get":
+        return isRoot
+          ? service.getCategories(event)
+          : service.getCategory(event);
+      case "put":
+        if (!isRoot) {
+          return service.editCategory(event);
+        }
+      case "delete":
+        if (!isRoot) {
+          return service.deleteCategory(event);
+        }
+    }
+
+    return ErrorResponse(404, "requetsted method not allowed");
+  }
+).use(service.conditionalBodyParser());
