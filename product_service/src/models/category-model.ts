ebc7a0b3b43f470d93f85440e7fe7e83c1800b79
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-15 04:55:48 +0100

    setup category model

---
+++ product_service/src/models/category-model.ts
@@ -0,0 +1,45 @@
+import { StringAttribute } from "aws-cdk-lib/aws-cognito";
+import mongoose from "mongoose";
+
+type CategoryModel = {
+  name: string;
+  nameTranslations: string;
+  ParentId: string;
+  subCategories: CategoryDoc[];
+  products: string[];
+  displayOrder: number;
+};
+
+export type CategoryDoc = mongoose.Document & CategoryModel;
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: String,
+    nameTranslations: { en: { type: String }, de: { type: String } },
+    parentId: { type: mongoose.SchemaTypes.ObjectId, ref: "categories" },
+    subCategories: [
+      {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "categories",
+      },
+    ],
+    products: [{ type: mongoose.SchemaTypes.ObjectId, ref: "products" }],
+    displayOrder: { type: Number, default: 1 },
+  },
+  {
+    toJSON: {
+      transform(doc, ret: { [key: string]: any }, options) {
+        delete ret.__v;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+      },
+    },
+    timestamps: true,
+  }
+);
+
+const categories =
+  mongoose.models.categories ||
+  mongoose.model<CategoryDoc>("categories", CategorySchema);
+
+export { categories };
