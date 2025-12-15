2399c64333a5442539db8a9cbe7741fd31fb0ac6
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-15 05:20:48 +0100

    added try catch block for proper error handling

--- product_service/src/repository/category-repository.ts
+++ product_service/src/repository/category-repository.ts
@@ -1,31 +1,35 @@
 import { CategoryInput } from "../dto/category-input";
 import { categories, CategoryDoc } from "../models/category-model";
-import { products } from "../models/product-models";
+import { InternalError } from "../utility/response";
 
 export class CategoryRepository {
   cconstructor() {}
 
   async createCategory({ name, parentId }: CategoryInput) {
-    // create a new category
-    const newCategory = await categories.create({
-      name,
-      parentId,
-      subcategory: [],
-      products: [],
-    });
-    // check parent id exists
-    // update parent category with the new category as child
-    if (parentId) {
-      const parentCategory = (await categories.findById(
-        parentId
-      )) as CategoryDoc;
-      parentCategory.subCategories = [
-        ...parentCategory.subCategories,
-        newCategory._id,
-      ];
-      await parentCategory.save();
+    try {
+      // create a new category
+      const newCategory = await categories.create({
+        name,
+        parentId,
+        subcategory: [],
+        products: [],
+      });
+      // check parent id exists
+      // update parent category with the new category as child
+      if (parentId) {
+        const parentCategory = (await categories.findById(
+          parentId
+        )) as CategoryDoc;
+        parentCategory.subCategories = [
+          ...parentCategory.subCategories,
+          newCategory._id,
+        ];
+        await parentCategory.save();
+      }
+      // return the created category
+      return newCategory;
+    } catch (error) {
+      InternalError(error);
     }
-    // return the created category
-    return newCategory;
   }
 }
