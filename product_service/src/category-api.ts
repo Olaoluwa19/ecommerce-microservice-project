47e4ec5e4c257e4910abe38275ea5079d6d5ac59
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-18 08:19:42 +0100

    modify handler to be sble to handle get request properly by adding httpEventNormalizer and updating the handling logic

--- product_service/src/category-api.ts
+++ product_service/src/category-api.ts
@@ -5,38 +5,46 @@ import { CategoryRepository } from "./repository/category-repository";
 import { connectDB } from "./utility/mongodb";
 import middy from "@middy/core";
 import jsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
 
 const service = new CategoryService(new CategoryRepository());
 
-export const handler = middy(
-  async (
-    event: APIGatewayEvent,
-    context: Context
-  ): Promise<APIGatewayProxyResult> => {
-    await connectDB();
+export const baseHandler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  await connectDB();
 
-    const isRoot = event.pathParameters === null;
+  const categoryId = event.pathParameters?.id;
+  const isRoot = !categoryId;
 
-    switch (event.httpMethod.toLowerCase()) {
-      case "post":
-        if (isRoot) {
-          return service.createCategory(event);
-        }
-        break;
-      case "get":
-        return isRoot
-          ? service.getCategories(event)
-          : service.getCategory(event);
-      case "put":
-        if (!isRoot) {
-          return service.editCategory(event);
-        }
-      case "delete":
-        if (!isRoot) {
-          return service.deleteCategory(event);
-        }
-    }
-
-    return service.ResponseWithError(event);
+  switch (event.httpMethod.toLowerCase()) {
+    case "post":
+      if (isRoot) {
+        return service.createCategory(event);
+      }
+      break;
+    case "get":
+      return isRoot ? service.getCategories(event) : service.getCategory(event);
+    case "put":
+      if (!isRoot) {
+        return service.editCategory(event);
+      }
+      break;
+    case "delete":
+      if (!isRoot) {
+        return service.deleteCategory(event);
+      }
+      break;
   }
-).use(service.conditionalBodyParser());
+
+  return service.ResponseWithError(event);
+};
+
+export const handler = middy(baseHandler)
+  .use(httpEventNormalizer())
+  .use(
+    jsonBodyParser({
+      disableContentTypeError: true, // Allows GET/DELETE with stray Content-Type: application/json
+    })
+  );
