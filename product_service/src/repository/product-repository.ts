35194ff48b105a4e2ff7744e0677ec232cbb755f
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-20 04:57:59 +0100

    made error handling more robust

--- product_service/src/repository/product-repository.ts
+++ product_service/src/repository/product-repository.ts
@@ -1,5 +1,6 @@
 import { ProductInput } from "../dto/product-input";
 import { ProductDoc, products } from "../models";
+import { BadRequest, InternalError, NotFound } from "../utility/response";
 
 export class ProductRepository {
   constructor() {}
@@ -11,25 +12,40 @@ export class ProductRepository {
     category_id,
     image_url,
   }: ProductInput): Promise<ProductDoc> {
-    return products.create({
-      name,
-      description,
-      price,
-      category_id,
-      image_url,
-      availability: true,
-    });
+    try {
+      return await products.create({
+        name,
+        description,
+        price,
+        category_id,
+        image_url,
+        availability: true,
+      });
+    } catch (error) {
+      throw InternalError(error);
+    }
   }
 
   async getAllProducts(offset = 0, pages?: number) {
-    return products
-      .find()
-      .skip(offset)
-      .limit(pages ? pages : 500);
+    try {
+      return products
+        .find()
+        .skip(offset)
+        .limit(pages ? pages : 500);
+    } catch (error) {
+      return InternalError(error);
+    }
   }
 
   async getProductById(id: string) {
-    return products.findById(id);
+    try {
+      if (!id) {
+        return BadRequest("Product ID is required");
+      }
+      return products.findById(id);
+    } catch (error) {
+      return InternalError(error);
+    }
   }
 
   async updateProduct({
@@ -41,21 +57,46 @@ export class ProductRepository {
     image_url,
     availability,
   }: ProductInput) {
-    let existingProduct = (await products.findById(id)) as ProductDoc;
+    try {
+      if (!id) {
+        return BadRequest("Product ID is required");
+      }
+      let existingProduct = (await products.findById(id)) as ProductDoc;
+      if (!existingProduct) {
+        return NotFound("Product not found");
+      }
+      existingProduct.name = name;
+      existingProduct.description = description;
+      existingProduct.price = price;
+      existingProduct.category_id = category_id;
+      existingProduct.image_url = image_url;
+      existingProduct.availability = availability;
 
-    existingProduct.name = name;
-    existingProduct.description = description;
-    existingProduct.price = price;
-    existingProduct.category_id = category_id;
-    existingProduct.image_url = image_url;
-    existingProduct.availability = availability;
-
-    return existingProduct.save();
+      return existingProduct.save();
+    } catch (error) {
+      return InternalError(error);
+    }
   }
 
   async deleteProduct(id: string) {
-    const { category_id } = (await products.findById(id)) as ProductDoc;
-    const deleteResult = await products.deleteOne({ _id: id });
-    return { deleteResult, category_id };
+    try {
+      if (!id) {
+        return BadRequest("Product ID is required");
+      }
+      const { category_id } = (await products.findById(id)) as ProductDoc;
+      if (!category_id) {
+        return NotFound("Category not found");
+      }
+
+      const product = await products.findById(id);
+      if (!product) {
+        return NotFound("Product not found");
+      }
+      const deleteResult = await products.deleteOne({ _id: product._id });
+
+      return { deleteResult, category_id };
+    } catch (error) {
+      return InternalError(error);
+    }
   }
 }
