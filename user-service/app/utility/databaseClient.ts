a6df7a4f72a78407853b422c183c0b7088dec15d
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-22 06:34:05 +0100

    fix: add error handling to database client initialization

--- user-service/app/utility/databaseClient.ts
+++ user-service/app/utility/databaseClient.ts
@@ -1,11 +1,15 @@
 import pg from "pg";
 
 export const DBClient = () => {
-  return new pg.Client({
-    host: "127.0.0.1",
-    user: "root",
-    database: "user_service",
-    password: "root",
-    port: 5432,
-  });
+  try {
+    return new pg.Client({
+      host: "127.0.0.1",
+      user: "root",
+      database: "user_service",
+      password: "root",
+      port: 5432,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
