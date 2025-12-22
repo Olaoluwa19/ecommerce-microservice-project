23ec3df3e097ff4eab8d75f3f7d876159b7b77da
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-22 06:34:27 +0100

    fix: add error handling to GenerateAccessCode and SendVerificationCode functions

--- user-service/app/utility/notification.ts
+++ user-service/app/utility/notification.ts
@@ -6,21 +6,29 @@ const authToken = process.env.authToken;
 const client = twilio(accountSid, authToken);
 
 export const GenerateAccessCode = () => {
-  const code = Math.floor(10000 + Math.random() * 900000);
-  let expiry = new Date();
-  expiry.setTime(new Date().getTime() + 30 * 60 * 1000); // 30 minutes from now
-  return { code, expiry };
+  try {
+    const code = Math.floor(10000 + Math.random() * 900000);
+    let expiry = new Date();
+    expiry.setTime(new Date().getTime() + 30 * 60 * 1000); // 30 minutes from now
+    return { code, expiry };
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 export const SendVerificationCode = async (
   code: number,
   toPhoneNumber: string
 ) => {
-  const response = await client.messages.create({
-    body: `Your verification code ${code}. It will expire within 30 minutes`,
-    from: "+13193492568",
-    to: toPhoneNumber.trim(),
-  });
-  console.log(response);
-  return response;
+  try {
+    const response = await client.messages.create({
+      body: `Your verification code ${code}. It will expire within 30 minutes`,
+      from: "+13193492568",
+      to: toPhoneNumber.trim(),
+    });
+    console.log(response);
+    return response;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
