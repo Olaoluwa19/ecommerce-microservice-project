69d8dc9080a8b784d0ec9063ffb3f4b4968ceae9
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-22 07:07:11 +0100

    fix: add error handling to executeQuery method in DBOperation class

--- user-service/app/repository/dbOperation.ts
+++ user-service/app/repository/dbOperation.ts
@@ -1,13 +1,18 @@
+import { InternalError } from "app/utility/response.js";
 import { DBClient } from "../utility/databaseClient.js";
 
 export class DBOperation {
   constructor() {}
 
   async executeQuery(queryString: string, values: unknown[]) {
-    const client = DBClient();
-    await client.connect();
-    const result = await client.query(queryString, values);
-    await client.end();
-    return result;
+    try {
+      const client = DBClient();
+      await client.connect();
+      const result = await client.query(queryString, values);
+      await client.end();
+      return result;
+    } catch (error) {
+      return InternalError(error);
+    }
   }
 }
