71cca4a651f33a8cfaa003590f4533d031f4a245
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-25 10:16:44 +0100

    feat: implement methods for cart operations in CartRepository

--- user-service/app/repository/cartRepository.ts
+++ user-service/app/repository/cartRepository.ts
@@ -5,11 +5,27 @@ import {
 } from "app/utility/response.js";
 import { AddressModel } from "../models/AddressModel.js";
 import { CartModel } from "../models/CartModel.js";
-import { ProfileInput } from "../models/dto/AddressInput.js";
+import { CartInput } from "../models/dto/CartInput.js";
 const { DBOperation } = await import("./dbOperation.js");
 
 export class CartRepository extends DBOperation {
   constructor() {
     super();
   }
+
+  async findCart(userId: number) {}
+
+  async createCart(userId: number) {}
+
+  async findCartItemById(cartId: number) {}
+
+  async findCartItemByProductId(productId: number) {}
+
+  async findCartItems(userId: number) {}
+
+  async updateCartItemById(itemId: number, qty: number) {}
+
+  async updateCartItemByProductId(productId: string, qty: number) {}
+
+  async deleteCartItem(id: number) {}
 }
