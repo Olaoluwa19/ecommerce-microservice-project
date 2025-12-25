1d236673f3f9949edb830dce116f1a0b14cf859e
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-25 10:33:59 +0100

    feat: added  findShoppingCart method and fix model import

--- user-service/app/repository/cartRepository.ts
+++ user-service/app/repository/cartRepository.ts
@@ -4,7 +4,7 @@ import {
   InternalError,
 } from "app/utility/response.js";
 import { AddressModel } from "../models/AddressModel.js";
-import { ShoppingCart } from "../models/ShoppingCartModel.js";
+import { ShoppingCartModel } from "../models/ShoppingCartModel.js";
 import { CartInput } from "../models/dto/CartInput.js";
 const { DBOperation } = await import("./dbOperation.js");
 
@@ -13,7 +13,15 @@ export class ShoppingCartRepository extends DBOperation {
     super();
   }
 
-  async findShoppingCart(userId: number) {}
+  async findShoppingCart(userId: number) {
+    const queryString =
+      "SELECT cart_id, user_id FROM shopping_carts WHERE user_id=$1";
+    const values = [userId];
+    const result = await this.executeQuery(queryString, values);
+    return result.rows.length > 0
+      ? (result.rows[0] as ShoppingCartModel)
+      : null;
+  }
 
   async createShoppingCart(userId: number) {}
 
