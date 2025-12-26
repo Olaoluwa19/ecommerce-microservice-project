97710ec824398086aade88a80f95d197a7468e30
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-26 22:55:36 +0100

    fix: removed dotenv import and clean up unnecessary import statements

--- user-service/app/service/paymentService.ts
+++ user-service/app/service/paymentService.ts
@@ -1,28 +1,8 @@
-import * as dotenv from "dotenv";
-dotenv.config(); // Loads .env into process.env
 import bodyParser from "@middy/http-json-body-parser";
 import { ErrorResponse, SuccessResponse } from "../utility/response.js";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { PaymentRepository } from "../repository/paymentRepository.js";
 import { autoInjectable } from "tsyringe";
-import { plainToClass } from "class-transformer";
-import { SignupInput } from "../models/dto/SignupInput.js";
-import { AppValidationError } from "../utility/errors.js";
-import {
-  GetSalt,
-  GetHashedPassword,
-  ValidatePassword,
-  GetToken,
-  VerifyToken,
-} from "../utility/password.js";
-import { LoginInput } from "../models/dto/LoginInput.js";
-import { VerificationInput } from "../models/dto/UpdateInput.js";
-import {
-  GenerateAccessCode,
-  SendVerificationCode,
-} from "../utility/notification.js";
-import { TimeDifference } from "../utility/dateHelper.js";
-import { ProfileInput } from "../models/dto/AddressInput.js";
 
 @autoInjectable()
 export class PaymentService {
