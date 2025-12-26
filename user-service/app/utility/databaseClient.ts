9ce1d8126fd34d4bf09535113459e6646e0cc697
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-26 22:56:44 +0100

    configured db to load vars from .env file

--- user-service/app/utility/databaseClient.ts
+++ user-service/app/utility/databaseClient.ts
@@ -3,11 +3,11 @@ import pg from "pg";
 export const DBClient = () => {
   try {
     return new pg.Client({
-      host: "127.0.0.1",
-      user: "root",
-      database: "user_service",
-      password: "root",
-      port: 5432,
+      host: process.env.host,
+      user: process.env.user,
+      database: process.env.database,
+      password: process.env.password,
+      port: process.env.port ? parseInt(process.env.port) : 5432,
     });
   } catch (error) {
     throw new Error(error);
