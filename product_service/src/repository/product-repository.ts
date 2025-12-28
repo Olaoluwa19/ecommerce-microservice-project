bf12e289cf088e3da9e31b9fc65ef3f499278ba5
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-28 03:45:27 +0100

    feat: made error handling more robust. Removed unnecesary http error responses in repository

--- product_service/src/repository/product-repository.ts
+++ product_service/src/repository/product-repository.ts
@@ -1,6 +1,5 @@
 import { ProductInput } from "../dto/product-input";
 import { ProductDoc, products } from "../models";
-import { BadRequest, InternalError, NotFound } from "../utility/response";
 
 export class ProductRepository {
   constructor() {}
@@ -12,40 +11,25 @@ export class ProductRepository {
     category_id,
     image_url,
   }: ProductInput): Promise<ProductDoc> {
-    try {
-      return await products.create({
-        name,
-        description,
-        price,
-        category_id,
-        image_url,
-        availability: true,
-      });
-    } catch (error) {
-      throw InternalError(error);
-    }
+    return await products.create({
+      name,
+      description,
+      price,
+      category_id,
+      image_url,
+      availability: true,
+    });
   }
 
   async getAllProducts(offset = 0, pages?: number) {
-    try {
-      return products
-        .find()
-        .skip(offset)
-        .limit(pages ? pages : 500);
-    } catch (error) {
-      return InternalError(error);
-    }
+    return products
+      .find()
+      .skip(offset)
+      .limit(pages ? pages : 500);
   }
 
   async getProductById(id: string) {
-    try {
-      if (!id) {
-        return BadRequest("Product ID is required");
-      }
-      return (await products.findById(id)) as ProductDoc;
-    } catch (error) {
-      return InternalError(error);
-    }
+    return (await products.findById(id)) as ProductDoc;
   }
 
   async updateProduct({
@@ -57,46 +41,26 @@ export class ProductRepository {
     image_url,
     availability,
   }: ProductInput) {
-    try {
-      if (!id) {
-        return BadRequest("Product ID is required");
-      }
-      let existingProduct = (await products.findById(id)) as ProductDoc;
-      if (!existingProduct) {
-        return NotFound("Product not found");
-      }
-      existingProduct.name = name;
-      existingProduct.description = description;
-      existingProduct.price = price;
-      existingProduct.category_id = category_id;
-      existingProduct.image_url = image_url;
-      existingProduct.availability = availability;
+    let existingProduct = (await products.findById(id)) as ProductDoc;
+    if (!existingProduct) throw new Error("Product not found");
+    existingProduct.name = name;
+    existingProduct.description = description;
+    existingProduct.price = price;
+    existingProduct.category_id = category_id;
+    existingProduct.image_url = image_url;
+    existingProduct.availability = availability;
 
-      return existingProduct.save();
-    } catch (error) {
-      return InternalError(error);
-    }
+    return existingProduct.save();
   }
 
   async deleteProduct(id: string) {
-    try {
-      if (!id) {
-        return BadRequest("Product ID is required");
-      }
-      const { category_id } = (await products.findById(id)) as ProductDoc;
-      if (!category_id) {
-        return NotFound("Category not found");
-      }
+    const { category_id } = (await products.findById(id)) as ProductDoc;
+    if (!category_id) throw new Error("Category ID not found for the product");
 
-      const product = await products.findById(id);
-      if (!product) {
-        return NotFound("Product not found");
-      }
-      const deleteResult = await products.deleteOne({ _id: product._id });
+    const product = await products.findById(id);
+    if (!product) throw new Error("Product not found");
+    const deleteResult = await products.deleteOne({ _id: product._id });
 
-      return { deleteResult, category_id };
-    } catch (error) {
-      return InternalError(error);
-    }
+    return { deleteResult, category_id };
   }
 }
