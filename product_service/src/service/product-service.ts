e817be537686d7f9e39e2e076ebb7dd5057eaab5
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-12-28 03:57:15 +0100

    feat: added try catch block and make error handling more robust

--- product_service/src/service/product-service.ts
+++ product_service/src/service/product-service.ts
@@ -25,91 +25,84 @@ export class ProductService {
   }
 
   async createProduct(event: APIGatewayEvent) {
-    if (!event.body) {
-      return BadRequest("Request body is required");
-    }
-
-    let payload;
     try {
-      payload = JSON.parse(event.body);
-    } catch (e) {
-      return BadRequest("Invalid JSON");
+      if (!event.body) {
+        return BadRequest("Request body is required");
+      }
+      let payload;
+      try {
+        payload = JSON.parse(event.body);
+      } catch (e) {
+        return BadRequest("Invalid JSON");
+      }
+      const input = plainToClass(ProductInput, payload);
+      const errors = await AppValidationError(input);
+      if (errors && errors.length > 0) {
+        console.log("Validation errors:", errors);
+        return BadRequest(errors);
+      }
+
+      const data = await this._repository.createProduct(input);
+      const categoryProduct = await new CategoryRepository().addItem({
+        id: input.category_id,
+        products: [data._id as string],
+      });
+      return CreatedResponse(categoryProduct);
+    } catch (error) {
+      return InternalError(error);
     }
-
-    const input = plainToClass(ProductInput, payload);
-    const errors = await AppValidationError(input);
-
-    if (errors && errors.length > 0) {
-      console.log("Validation errors:", errors);
-      return BadRequest(errors);
-    }
-
-    const data = await this._repository.createProduct(input);
-    if (!data) {
-      return InternalError("Failed to create product");
-    }
-
-    const categoryProduct = await new CategoryRepository().addItem({
-      id: input.category_id,
-      products: [data._id as string],
-    });
-    return CreatedResponse(categoryProduct);
   }
 
   async getProducts(event: APIGatewayEvent) {
-    const data = await this._repository.getAllProducts();
-    if ("statusCode" in data) {
-      return data; // It's an error response
+    try {
+      const data = await this._repository.getAllProducts();
+      if (data.length === 0) {
+        return NotFound("No products found");
+      }
+      return SuccessResponse(data);
+    } catch (error) {
+      return InternalError(error);
     }
-    if (data.length === 0) {
-      return NotFound("No products found");
-    }
-    return SuccessResponse(data);
   }
 
   async getProduct(event: APIGatewayEvent) {
-    const productId = event.pathParameters?.id;
-    if (!productId) return BadRequest("Product id is required");
-    const data = await this._repository.getProductById(productId);
-    if ("statusCode" in data) {
-      return data; // It's an error response
+    try {
+      const productId = event.pathParameters?.id;
+      if (!productId) return BadRequest("Product id is required");
+      const data = await this._repository.getProductById(productId);
+      if (!data) {
+        return NotFound("No products found");
+      }
+      return SuccessResponse(data);
+    } catch (error) {
+      return InternalError(error);
     }
-
-    if (!data) {
-      return NotFound("No products found");
-    }
-    return SuccessResponse(data);
   }
 
   async editProduct(event: APIGatewayEvent) {
-    const productId = event.pathParameters?.id;
-    if (!productId) return BadRequest("Product id is required");
-    const input = plainToClass(ProductInput, JSON.parse(event.body!));
-    const errors = await AppValidationError(input);
+    try {
+      const productId = event.pathParameters?.id;
+      if (!productId) return BadRequest("Product id is required");
+      const input = plainToClass(ProductInput, JSON.parse(event.body!));
+      const errors = await AppValidationError(input);
+      if (errors && errors.length > 0) {
+        console.log("Validation errors:", errors);
+        return BadRequest(errors);
+      }
 
-    if (errors && errors.length > 0) {
-      console.log("Validation errors:", errors);
-      return BadRequest(errors);
+      input.id = productId;
+      const data = await this._repository.updateProduct(input);
+      return CreatedResponse(data);
+    } catch (error) {
+      return InternalError(error);
     }
-
-    input.id = productId;
-
-    const data = await this._repository.updateProduct(input);
-
-    return CreatedResponse(data);
   }
 
   async deleteProduct(event: APIGatewayEvent) {
     try {
       const productId = event.pathParameters?.id;
       if (!productId) return BadRequest("Product id is required");
-
       const result = await this._repository.deleteProduct(productId);
-
-      if ("statusCode" in result) {
-        return result;
-      }
-
       const { deleteResult, category_id } = result;
 
       await new CategoryRepository().removeItem({
@@ -128,23 +121,16 @@ export class ProductService {
       if (!event.body) {
         return BadRequest("Request body is required");
       }
-
       const input = plainToClass(ServiceInput, event.body);
-      const errors = await AppValidationError(input);
 
+      const errors = await AppValidationError(input);
       if (errors && errors.length > 0) {
         console.log("Validation errors:", errors);
         return BadRequest(errors);
       }
-
       console.log("Service Input:", input);
 
       const data = await this._repository.getProductById(input.productId);
-
-      if ("statusCode" in data) {
-        return data;
-      }
-
       const { _id, name, price, image_url } = data;
       console.log("Product Data:", data);
 
