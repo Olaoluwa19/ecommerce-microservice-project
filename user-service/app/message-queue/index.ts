cebafa288c8146e68b98a22638615e0e17b1d983
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-01-02 07:47:37 +0100

    fix: corrected PRODUCT_URL

--- user-service/app/message-queue/index.ts
+++ user-service/app/message-queue/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const PRODUCT_URL = "https://127.0.0.1:3000/product-queue";
+const PRODUCT_URL = "http://127.0.0.1:3000/product-queue";
 
 export const PullData = async (requestData: Record<string, unknown>) => {
   return axios.post(PRODUCT_URL, requestData);
