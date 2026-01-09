ee6bdd80595f4c8e21b5c38d84297f53b42172ba
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-01-09 01:48:35 +0100

    feat: added deployed product service url to message queue

--- user-service/app/message-queue/index.ts
+++ user-service/app/message-queue/index.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const PRODUCT_URL = "http://127.0.0.1:3000/product-queue";
+const PRODUCT_URL =
+  "https://wvuocjmqdd.execute-api.us-east-1.amazonaws.com/prod/product-queue";
+
+// "http://127.0.0.1:3000/product-queue";
 
 export const PullData = async (requestData: Record<string, unknown>) => {
   return axios.post(PRODUCT_URL, requestData);
