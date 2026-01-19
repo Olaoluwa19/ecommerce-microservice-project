24579db743fe9abc05d57379e0c91b3f4e03a07d
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-01-19 02:32:10 +0100

    feat: added findCartItems, updateCartItemById, deleteCartItem methods

--- user-service/app/repository/cartRepository.ts
+++ user-service/app/repository/cartRepository.ts
@@ -34,7 +34,20 @@ export class ShoppingCartRepository extends DBOperation {
     return result.rowCount > 0 ? (result.rows[0] as CartItemModel) : null;
   }
 
-  async findCartItems(userId: number) {}
+  async findCartItems(userId: number) {
+    const queryString = `SELECT 
+    ci.cart_id,
+    ci.item_id,
+    ci.product_id,
+    ci.name,
+    ci.price,
+    ci.item_qty,
+    ci.image_url, 
+    ci.created_at FROM shopping_carts sc INNER JOIN cart_items ci ON sc.cart_id = ci.cart_id WHERE sc.user_id=$1`;
+    const values = [userId];
+    const result = await this.executeQuery(queryString, values);
+    return result.rowCount > 0 ? (result.rows as CartItemModel[]) : [];
+  }
 
   async findCartItemsByCartId(cartId: number) {
     const queryString =
@@ -59,7 +72,13 @@ export class ShoppingCartRepository extends DBOperation {
     return result.rowCount > 0 ? (result.rows[0] as CartItemModel) : null;
   }
 
-  async updateCartItemById(itemId: number, qty: number) {}
+  async updateCartItemById(itemId: number, qty: number) {
+    const queryString =
+      "UPDATE cart_items SET item_qty=$1 WHERE item_id=$2 RETURNING *";
+    const values = [qty, itemId];
+    const result = await this.executeQuery(queryString, values);
+    return result.rowCount > 0 ? (result.rows[0] as CartItemModel) : null;
+  }
 
   async updateCartItemByProductId(productId: string, qty: number) {
     const queryString =
@@ -69,5 +88,9 @@ export class ShoppingCartRepository extends DBOperation {
     return result.rowCount > 0 ? (result.rows[0] as CartItemModel) : null;
   }
 
-  async deleteCartItem(id: number) {}
+  async deleteCartItem(id: number) {
+    const queryString = "DELETE FROM cart_items WHERE item_id=$1";
+    const values = [id];
+    return this.executeQuery(queryString, values);
+  }
 }
