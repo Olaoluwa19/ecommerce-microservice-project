d63cd8bae6fdb42a2eafed420ac59ded04178ceb
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-01-20 03:51:22 +0100

    feat: configured transaction stack

--- transaction-service/lib/transaction-service-stack.ts
+++ transaction-service/lib/transaction-service-stack.ts
@@ -1,16 +1,63 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import { SubscriptionFilter, Topic } from "aws-cdk-lib/aws-sns";
+import { SqsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import { join } from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TransactionServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const orderQueue = new Queue(this, "order_queue", {
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TransactionServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const orderTopic = Topic.fromTopicArn(
+      this,
+      "order-consume-topic",
+      cdk.Fn.importValue("customer-topic"),
+    );
+
+    orderTopic.addSubscription(
+      new SqsSubscription(orderQueue, {
+        rawMessageDelivery: true,
+        filterPolicy: {
+          actionType: SubscriptionFilter.stringFilter({
+            allowlist: ["place_order"],
+          }),
+        },
+      }),
+    );
+
+    // handler
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: ["aws-sdk"],
+      },
+      runtime: Runtime.NODEJS_18_X,
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        ORDER_QUEUE_URL: orderQueue.queueUrl,
+      },
+    };
+
+    const createOrderHandler = new NodejsFunction(
+      this,
+      "create-order-handler",
+      {
+        entry: join(__dirname, "..", "src", "order", "create.ts"),
+        ...nodeJsFunctionProps,
+      },
+    );
+
+    createOrderHandler.addEventSource(new SqsEventSource(orderQueue));
   }
 }
