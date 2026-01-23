ab1a71b8c06e8c774a506ab04962af0c09b44547
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-01-23 04:55:45 +0100

    feat: break the userHandlers into piece by piece to handle the logics independently

--- user-service/app/handlers/userHandler.ts
+++ user-service/app/handlers/userHandler.ts
@@ -1,65 +1,33 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import middy from "@middy/core";
-import bodyParser from "@middy/http-json-body-parser";
 import { container } from "../container.js";
 
-const { userService, cartService, paymentService } = container;
+const { userService } = container;
 
 export const Signup = middy((event: APIGatewayProxyEventV2) => {
   return userService.CreateUser(event);
-}).use(bodyParser());
+}).use(userService.conditionalBodyParser());
 
 export const Login = middy((event: APIGatewayProxyEventV2) => {
   return userService.UserLogin(event);
-}).use(bodyParser());
+}).use(userService.conditionalBodyParser());
+
+export const GetVerificationCode = middy((event: APIGatewayProxyEventV2) => {
+  return userService.GetVerificationToken(event);
+}).use(userService.conditionalBodyParser());
 
 export const Verify = middy((event: APIGatewayProxyEventV2) => {
-  const httpMethod = event.requestContext.http.method.toLowerCase();
-  if (httpMethod === "post") {
-    return userService.VerifyUser(event);
-  } else if (httpMethod === "get") {
-    return userService.GetVerificationToken(event);
-  } else {
-    return userService.ResponseWithError(event);
-  }
+  return userService.VerifyUser(event);
 }).use(userService.conditionalBodyParser());
 
-export const Profile = middy((event: APIGatewayProxyEventV2) => {
-  const httpMethod = event.requestContext.http.method.toLowerCase();
-  if (httpMethod === "post") {
-    return userService.CreateProfile(event);
-  } else if (httpMethod === "put") {
-    return userService.EditProfile(event);
-  } else if (httpMethod === "get") {
-    return userService.GetProfile(event);
-  } else {
-    return userService.ResponseWithError(event);
-  }
+export const CreateProfile = middy((event: APIGatewayProxyEventV2) => {
+  return userService.CreateProfile(event);
 }).use(userService.conditionalBodyParser());
 
-export const Cart = middy((event: APIGatewayProxyEventV2) => {
-  const httpMethod = event.requestContext.http.method.toLowerCase();
-  if (httpMethod === "post") {
-    return cartService.CreateCart(event);
-  } else if (httpMethod === "put") {
-    return cartService.UpdateCart(event);
-  } else if (httpMethod === "get") {
-    return cartService.GetCart(event);
-  } else if (httpMethod === "delete") {
-    return cartService.DeleteCart(event);
-  } else {
-    return cartService.ResponseWithError(event);
-  }
-}).use(cartService.conditionalBodyParser());
+export const EditProfile = middy((event: APIGatewayProxyEventV2) => {
+  return userService.EditProfile(event);
+}).use(userService.conditionalBodyParser());
 
-export const CollectPayment = middy((event: APIGatewayProxyEventV2) => {
-  return cartService.CollectPayment(event);
-}).use(cartService.conditionalBodyParser());
-
-export const Payment = middy((event: APIGatewayProxyEventV2) => {
-  const httpMethod = event.requestContext.http.method.toLowerCase();
-  if (httpMethod === "post") return paymentService.CreatePaymentMethod(event);
-  if (httpMethod === "put") return paymentService.UpdatePaymentMethod(event);
-  if (httpMethod === "get") return paymentService.GetPaymentMethod(event);
-  return paymentService.ResponseWithError(event);
-}).use(paymentService.conditionalBodyParser());
+export const GetProfile = middy((event: APIGatewayProxyEventV2) => {
+  return userService.GetProfile(event);
+}).use(userService.conditionalBodyParser());
