6b370f562da0c5185f76389bfc06fb8089d76143
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-03-07 03:12:15 +0100

    feat: added body-parser

--- user-service/app/handlers/paymentHandler.ts
+++ user-service/app/handlers/paymentHandler.ts
@@ -1,18 +1,17 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import middy from "@middy/core";
-import bodyParser from "@middy/http-json-body-parser";
 import { container } from "../container.js";
 
 const { paymentService } = container;
 
 export const CreatePayment = middy((event: APIGatewayProxyEventV2) => {
   return paymentService.CreatePaymentMethod(event);
-}).use(bodyParser());
+}).use(paymentService.conditionalBodyParser());
 
 export const EditPayment = middy((event: APIGatewayProxyEventV2) => {
   return paymentService.UpdatePaymentMethod(event);
-}).use(bodyParser());
+}).use(paymentService.conditionalBodyParser());
 
 export const GetPayment = middy((event: APIGatewayProxyEventV2) => {
   return paymentService.GetPaymentMethod(event);
-}).use(bodyParser());
+}).use(paymentService.conditionalBodyParser());
